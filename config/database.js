cec8c3e37f974b38f1d27048bd2dce0fcedf5d4a
MarketingGoemezPalacio <servicios@webgomezpalacio.com>
2022-07-26 11:46:08 -0500

    version-one

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
  
 const db = new Sequelize(process.env.DBNAME, process.env.USER, process.env.PASSWORD, {
-    host: "localhost",
+    host: process.env.DBHOST,
     dialect: "mysql",
     "define": {
         "timestamps": false
